e173359a952ec312ee76203a5a30cf7f0ed8a42b
Josh Gumerove <josh.gumerove@gmail.com>
2020-11-18 20:35:27 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,7 +1,7 @@
-def loop_message_five_times
-  puts "hello world"
-  puts "hello world"
-  puts "hello world"
-  puts "hello world"
-  puts "hello world"
+def loop_message_five_times(message)
+  count = 0
+  while count < 5 do
+    puts message
+    count += 1
+  end
 end
